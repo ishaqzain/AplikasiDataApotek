01c9b29dbaf59dcf8792be4f2a527769a82af5fb
Muchamad Ishaq Z <ishaq.muchamad@gmail.com>
2024-01-05 20:18:15 +0700

    add: frm jenis barang

---
+++ FrmJenisObat.Designer.vb
@@ -0,0 +1,152 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmJenisObat
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmJenisObat))
+        Me.DGJenisObat = New System.Windows.Forms.DataGridView()
+        Me.txtJenisObat = New System.Windows.Forms.TextBox()
+        Me.txtKodeJenis = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BtnHapus = New System.Windows.Forms.Button()
+        Me.BtnUbah = New System.Windows.Forms.Button()
+        Me.BtnSimpan = New System.Windows.Forms.Button()
+        CType(Me.DGJenisObat, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DGJenisObat
+        '
+        Me.DGJenisObat.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGJenisObat.Location = New System.Drawing.Point(45, 182)
+        Me.DGJenisObat.Margin = New System.Windows.Forms.Padding(6)
+        Me.DGJenisObat.Name = "DGJenisObat"
+        Me.DGJenisObat.RowHeadersWidth = 82
+        Me.DGJenisObat.Size = New System.Drawing.Size(785, 323)
+        Me.DGJenisObat.TabIndex = 15
+        '
+        'txtJenisObat
+        '
+        Me.txtJenisObat.Location = New System.Drawing.Point(188, 100)
+        Me.txtJenisObat.Margin = New System.Windows.Forms.Padding(6)
+        Me.txtJenisObat.Name = "txtJenisObat"
+        Me.txtJenisObat.Size = New System.Drawing.Size(196, 31)
+        Me.txtJenisObat.TabIndex = 14
+        '
+        'txtKodeJenis
+        '
+        Me.txtKodeJenis.Location = New System.Drawing.Point(188, 40)
+        Me.txtKodeJenis.Margin = New System.Windows.Forms.Padding(6)
+        Me.txtKodeJenis.Name = "txtKodeJenis"
+        Me.txtKodeJenis.Size = New System.Drawing.Size(196, 31)
+        Me.txtKodeJenis.TabIndex = 13
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(40, 106)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(115, 25)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Jenis Obat"
+        '
+        'Label1
+        '
+        Me.Label1.AutoEllipsis = True
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(40, 46)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(119, 25)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Kode Jenis"
+        '
+        'BtnHapus
+        '
+        Me.BtnHapus.Image = CType(resources.GetObject("BtnHapus.Image"), System.Drawing.Image)
+        Me.BtnHapus.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.BtnHapus.Location = New System.Drawing.Point(718, 31)
+        Me.BtnHapus.Margin = New System.Windows.Forms.Padding(6)
+        Me.BtnHapus.Name = "BtnHapus"
+        Me.BtnHapus.Size = New System.Drawing.Size(112, 104)
+        Me.BtnHapus.TabIndex = 10
+        Me.BtnHapus.Text = "Hapus"
+        Me.BtnHapus.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.BtnHapus.UseVisualStyleBackColor = True
+        '
+        'BtnUbah
+        '
+        Me.BtnUbah.Image = CType(resources.GetObject("BtnUbah.Image"), System.Drawing.Image)
+        Me.BtnUbah.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.BtnUbah.Location = New System.Drawing.Point(594, 31)
+        Me.BtnUbah.Margin = New System.Windows.Forms.Padding(6)
+        Me.BtnUbah.Name = "BtnUbah"
+        Me.BtnUbah.Size = New System.Drawing.Size(112, 104)
+        Me.BtnUbah.TabIndex = 9
+        Me.BtnUbah.Text = "Ubah"
+        Me.BtnUbah.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.BtnUbah.UseVisualStyleBackColor = True
+        '
+        'BtnSimpan
+        '
+        Me.BtnSimpan.Image = CType(resources.GetObject("BtnSimpan.Image"), System.Drawing.Image)
+        Me.BtnSimpan.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.BtnSimpan.Location = New System.Drawing.Point(470, 31)
+        Me.BtnSimpan.Margin = New System.Windows.Forms.Padding(6)
+        Me.BtnSimpan.Name = "BtnSimpan"
+        Me.BtnSimpan.Size = New System.Drawing.Size(112, 104)
+        Me.BtnSimpan.TabIndex = 8
+        Me.BtnSimpan.Text = "Simpan"
+        Me.BtnSimpan.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.BtnSimpan.UseVisualStyleBackColor = True
+        '
+        'FrmJenisObat
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(879, 546)
+        Me.Controls.Add(Me.DGJenisObat)
+        Me.Controls.Add(Me.txtJenisObat)
+        Me.Controls.Add(Me.txtKodeJenis)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnHapus)
+        Me.Controls.Add(Me.BtnUbah)
+        Me.Controls.Add(Me.BtnSimpan)
+        Me.Name = "FrmJenisObat"
+        Me.Text = "Jenis Obat"
+        CType(Me.DGJenisObat, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DGJenisObat As DataGridView
+    Friend WithEvents txtJenisObat As TextBox
+    Friend WithEvents txtKodeJenis As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents BtnHapus As Button
+    Friend WithEvents BtnUbah As Button
+    Friend WithEvents BtnSimpan As Button
+End Class
---
+++ FrmJenisObat.vb
@@ -0,0 +1,3 @@
+﻿Public Class FrmJenisObat
+
+End Class
