1ddfb9c556f7bc47e5f6a61971feeb3ff25a13cf
Muchamad Ishaq Z <ishaq.muchamad@gmail.com>
2024-01-06 10:42:25 +0700

    add: Frm user

---
+++ FrmUser.Designer.vb
@@ -0,0 +1,258 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmUser
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmUser))
+        Me.BtnTutup = New System.Windows.Forms.Button()
+        Me.BtnBatal = New System.Windows.Forms.Button()
+        Me.BtnHapus = New System.Windows.Forms.Button()
+        Me.BtnUbah = New System.Windows.Forms.Button()
+        Me.BtnSimpan = New System.Windows.Forms.Button()
+        Me.BtnCari = New System.Windows.Forms.Button()
+        Me.DgPengguna = New System.Windows.Forms.DataGridView()
+        Me.CboLevel = New System.Windows.Forms.ComboBox()
+        Me.TxtUlangPassword = New System.Windows.Forms.TextBox()
+        Me.TxtPassword = New System.Windows.Forms.TextBox()
+        Me.TxtNmPengguna = New System.Windows.Forms.TextBox()
+        Me.TxtKdPengguna = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.DgPengguna, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'BtnTutup
+        '
+        Me.BtnTutup.Image = CType(resources.GetObject("BtnTutup.Image"), System.Drawing.Image)
+        Me.BtnTutup.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.BtnTutup.Location = New System.Drawing.Point(1024, 160)
+        Me.BtnTutup.Margin = New System.Windows.Forms.Padding(6)
+        Me.BtnTutup.Name = "BtnTutup"
+        Me.BtnTutup.Size = New System.Drawing.Size(112, 104)
+        Me.BtnTutup.TabIndex = 69
+        Me.BtnTutup.Text = "Tutup"
+        Me.BtnTutup.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.BtnTutup.UseVisualStyleBackColor = True
+        '
+        'BtnBatal
+        '
+        Me.BtnBatal.Image = CType(resources.GetObject("BtnBatal.Image"), System.Drawing.Image)
+        Me.BtnBatal.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.BtnBatal.Location = New System.Drawing.Point(900, 160)
+        Me.BtnBatal.Margin = New System.Windows.Forms.Padding(6)
+        Me.BtnBatal.Name = "BtnBatal"
+        Me.BtnBatal.Size = New System.Drawing.Size(112, 104)
+        Me.BtnBatal.TabIndex = 68
+        Me.BtnBatal.Text = "Batal"
+        Me.BtnBatal.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.BtnBatal.UseVisualStyleBackColor = True
+        '
+        'BtnHapus
+        '
+        Me.BtnHapus.Image = CType(resources.GetObject("BtnHapus.Image"), System.Drawing.Image)
+        Me.BtnHapus.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.BtnHapus.Location = New System.Drawing.Point(776, 160)
+        Me.BtnHapus.Margin = New System.Windows.Forms.Padding(6)
+        Me.BtnHapus.Name = "BtnHapus"
+        Me.BtnHapus.Size = New System.Drawing.Size(112, 104)
+        Me.BtnHapus.TabIndex = 67
+        Me.BtnHapus.Text = "Hapus"
+        Me.BtnHapus.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.BtnHapus.UseVisualStyleBackColor = True
+        '
+        'BtnUbah
+        '
+        Me.BtnUbah.Image = CType(resources.GetObject("BtnUbah.Image"), System.Drawing.Image)
+        Me.BtnUbah.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.BtnUbah.Location = New System.Drawing.Point(652, 160)
+        Me.BtnUbah.Margin = New System.Windows.Forms.Padding(6)
+        Me.BtnUbah.Name = "BtnUbah"
+        Me.BtnUbah.Size = New System.Drawing.Size(112, 104)
+        Me.BtnUbah.TabIndex = 66
+        Me.BtnUbah.Text = "Ubah"
+        Me.BtnUbah.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.BtnUbah.UseVisualStyleBackColor = True
+        '
+        'BtnSimpan
+        '
+        Me.BtnSimpan.Image = CType(resources.GetObject("BtnSimpan.Image"), System.Drawing.Image)
+        Me.BtnSimpan.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.BtnSimpan.Location = New System.Drawing.Point(528, 160)
+        Me.BtnSimpan.Margin = New System.Windows.Forms.Padding(6)
+        Me.BtnSimpan.Name = "BtnSimpan"
+        Me.BtnSimpan.Size = New System.Drawing.Size(112, 104)
+        Me.BtnSimpan.TabIndex = 65
+        Me.BtnSimpan.Text = "Simpan"
+        Me.BtnSimpan.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.BtnSimpan.UseVisualStyleBackColor = True
+        '
+        'BtnCari
+        '
+        Me.BtnCari.Location = New System.Drawing.Point(529, 27)
+        Me.BtnCari.Name = "BtnCari"
+        Me.BtnCari.Size = New System.Drawing.Size(108, 79)
+        Me.BtnCari.TabIndex = 64
+        Me.BtnCari.Text = "Cari"
+        Me.BtnCari.UseVisualStyleBackColor = True
+        '
+        'DgPengguna
+        '
+        Me.DgPengguna.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DgPengguna.Location = New System.Drawing.Point(33, 324)
+        Me.DgPengguna.Name = "DgPengguna"
+        Me.DgPengguna.RowHeadersWidth = 82
+        Me.DgPengguna.RowTemplate.Height = 33
+        Me.DgPengguna.Size = New System.Drawing.Size(1103, 310)
+        Me.DgPengguna.TabIndex = 63
+        '
+        'CboLevel
+        '
+        Me.CboLevel.FormattingEnabled = True
+        Me.CboLevel.Location = New System.Drawing.Point(211, 231)
+        Me.CboLevel.Name = "CboLevel"
+        Me.CboLevel.Size = New System.Drawing.Size(261, 33)
+        Me.CboLevel.TabIndex = 62
+        '
+        'TxtUlangPassword
+        '
+        Me.TxtUlangPassword.Location = New System.Drawing.Point(211, 180)
+        Me.TxtUlangPassword.Name = "TxtUlangPassword"
+        Me.TxtUlangPassword.Size = New System.Drawing.Size(261, 31)
+        Me.TxtUlangPassword.TabIndex = 61
+        '
+        'TxtPassword
+        '
+        Me.TxtPassword.Location = New System.Drawing.Point(211, 129)
+        Me.TxtPassword.Name = "TxtPassword"
+        Me.TxtPassword.Size = New System.Drawing.Size(261, 31)
+        Me.TxtPassword.TabIndex = 60
+        '
+        'TxtNmPengguna
+        '
+        Me.TxtNmPengguna.Location = New System.Drawing.Point(211, 78)
+        Me.TxtNmPengguna.Name = "TxtNmPengguna"
+        Me.TxtNmPengguna.Size = New System.Drawing.Size(261, 31)
+        Me.TxtNmPengguna.TabIndex = 59
+        '
+        'TxtKdPengguna
+        '
+        Me.TxtKdPengguna.Location = New System.Drawing.Point(211, 27)
+        Me.TxtKdPengguna.Name = "TxtKdPengguna"
+        Me.TxtKdPengguna.Size = New System.Drawing.Size(261, 31)
+        Me.TxtKdPengguna.TabIndex = 58
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(28, 234)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(56, 25)
+        Me.Label5.TabIndex = 57
+        Me.Label5.Text = "Role"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(28, 183)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(168, 25)
+        Me.Label4.TabIndex = 56
+        Me.Label4.Text = "Ulang Password"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(28, 132)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(106, 25)
+        Me.Label3.TabIndex = 55
+        Me.Label3.Text = "Password"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(28, 81)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(110, 25)
+        Me.Label2.TabIndex = 54
+        Me.Label2.Text = "Username"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(28, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 25)
+        Me.Label1.TabIndex = 53
+        Me.Label1.Text = "User ID"
+        '
+        'FrmUser
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1172, 666)
+        Me.Controls.Add(Me.BtnTutup)
+        Me.Controls.Add(Me.BtnBatal)
+        Me.Controls.Add(Me.BtnHapus)
+        Me.Controls.Add(Me.BtnUbah)
+        Me.Controls.Add(Me.BtnSimpan)
+        Me.Controls.Add(Me.BtnCari)
+        Me.Controls.Add(Me.DgPengguna)
+        Me.Controls.Add(Me.CboLevel)
+        Me.Controls.Add(Me.TxtUlangPassword)
+        Me.Controls.Add(Me.TxtPassword)
+        Me.Controls.Add(Me.TxtNmPengguna)
+        Me.Controls.Add(Me.TxtKdPengguna)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "FrmUser"
+        Me.Text = "Form User"
+        CType(Me.DgPengguna, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnTutup As Button
+    Friend WithEvents BtnBatal As Button
+    Friend WithEvents BtnHapus As Button
+    Friend WithEvents BtnUbah As Button
+    Friend WithEvents BtnSimpan As Button
+    Friend WithEvents BtnCari As Button
+    Friend WithEvents DgPengguna As DataGridView
+    Friend WithEvents CboLevel As ComboBox
+    Friend WithEvents TxtUlangPassword As TextBox
+    Friend WithEvents TxtPassword As TextBox
+    Friend WithEvents TxtNmPengguna As TextBox
+    Friend WithEvents TxtKdPengguna As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ FrmUser.vb
@@ -0,0 +1,171 @@
+﻿Public Class FrmUser
+    Dim SQL As String
+    Dim Proses As New ClsKoneksi
+    Dim tblUser As DataTable
+
+    Sub Data_Record()
+        tblUser = Proses.ExecuteQuery("Select * From TblUser")
+        DgPengguna.DataSource = tblUser
+        DgPengguna.Columns(1).Width = 200
+        DgPengguna.Columns(2).Visible = False
+    End Sub
+
+    Sub Kode_Otomatis()
+        tblUser = Proses.ExecuteQuery("Select * From TblUser order by User_Id desc")
+        If tblUser.Rows.Count = 0 Then
+            TxtKdPengguna.Text = "USR-001"
+        Else
+            With tblUser.Rows(0)
+                TxtKdPengguna.Text = .Item("User_Id")
+                TxtKdPengguna.Focus()
+            End With
+            TxtKdPengguna.Text = Val(Microsoft.VisualBasic.Mid(TxtKdPengguna.Text, 5, 3)) + 1
+            If Len(TxtKdPengguna.Text) = 1 Then
+                TxtKdPengguna.Text = "USR-00" & TxtKdPengguna.Text & ""
+
+            ElseIf Len(TxtKdPengguna.Text) = 2 Then
+                TxtKdPengguna.Text = "USR-0" & TxtKdPengguna.Text & ""
+            ElseIf Len(TxtKdPengguna.Text) = 3 Then
+                TxtKdPengguna.Text = "USR-" & TxtKdPengguna.Text & ""
+            End If
+        End If
+    End Sub
+
+    Sub Bersih()
+        TxtKdPengguna.Enabled = True
+        TxtKdPengguna.Text = ""
+        TxtNmPengguna.Text = ""
+        Call Kode_Otomatis()
+        Call Data_Record()
+        BtnSimpan.Enabled = True
+        BtnUbah.Enabled = False
+        BtnHapus.Enabled = False
+        BtnBatal.Enabled = False
+        BtnTutup.Enabled = True
+        TxtKdPengguna.Enabled = False
+        TxtPassword.Enabled = True
+        TxtUlangPassword.Enabled = True
+        TxtPassword.Text = ""
+        TxtUlangPassword.Text = ""
+    End Sub
+
+    Private Sub FrmUser_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+        Call Data_Record()
+    End Sub
+
+    Private Sub btnSimpan_Click(sender As Object, e As EventArgs) Handles BtnSimpan.Click
+        If TxtNmPengguna.Text = "" Then
+            TxtNmPengguna.Focus()
+            Exit Sub
+        End If
+        If TxtPassword.Text = "" Then
+            TxtPassword.Focus()
+            Exit Sub
+        End If
+        If TxtUlangPassword.Text = "" Then
+            TxtUlangPassword.Focus()
+            Exit Sub
+        End If
+        If CboLevel.Text = "" Then
+            CboLevel.Focus()
+            Exit Sub
+        End If
+        If TxtPassword.Text <> TxtUlangPassword.Text Then
+            MsgBox("Ulangi, password keliru!!!", MsgBoxStyle.Critical, "Password")
+            TxtUlangPassword.Focus()
+            Exit Sub
+        Else
+            SQL = "Insert Into TblUser values ('" & TxtKdPengguna.Text & "','" & TxtNmPengguna.Text & "','" & TxtPassword.Text & "','" & CboLevel.Text & "')"
+            Proses.ExecuteNonQuery(SQL)
+            MessageBox.Show("Data Baru telah disimpan..!!", "Penyimpanan Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Call Bersih()
+        End If
+    End Sub
+
+    Private Sub BtnUbah_Click(sender As Object, e As EventArgs) Handles BtnUbah.Click
+        If TxtNmPengguna.Text = "" Then
+            TxtNmPengguna.Focus()
+            Exit Sub
+        End If
+        If CboLevel.Text = "" Then
+            CboLevel.Focus()
+            Exit Sub
+        Else
+            SQL = "Update TblUser Set Username = '" & TxtNmPengguna.Text & "',Role =  '" & CboLevel.Text & "' Where User_Id = '" & TxtKdPengguna.Text & "'"
+            Proses.ExecuteNonQuery(SQL)
+            MessageBox.Show("Data telah diubah..!!", "Perubahan Data Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Call Bersih()
+        End If
+
+    End Sub
+
+    Private Sub BtnBatal_Click(sender As Object, e As EventArgs) Handles BtnBatal.Click
+        Call Bersih()
+        TxtKdPengguna.Focus()
+    End Sub
+
+    Private Sub BtnHapus_Click(sender As Object, e As EventArgs) Handles BtnHapus.Click
+        SQL = "Delete From TblUser Where User_Id = '" & TxtKdPengguna.Text & "'"
+        Proses.ExecuteNonQuery(SQL)
+        MessageBox.Show("Data telah dihapus..!!", "Penghapusan Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Call Bersih()
+    End Sub
+
+    Private Sub BtnTutup_Click(sender As Object, e As EventArgs) Handles BtnTutup.Click
+        Me.Close()
+    End Sub
+
+    Private Sub BtnCari_Click(sender As Object, e As EventArgs) Handles BtnCari.Click
+        TxtKdPengguna.Text = InputBox("Masukkan User Id !!!")
+        If TxtKdPengguna.Text = "" Then
+            Call Kode_Otomatis()
+            TxtNmPengguna.Focus()
+            Exit Sub
+        End If
+
+        tblUser = Proses.ExecuteQuery("Select * From TblUser Where User_Id = '" & TxtKdPengguna.Text & "'")
+        If tblUser.Rows.Count = 0 Then
+            MessageBox.Show("Data Pengguna Tidak Ditemukan..!!", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            TxtNmPengguna.Focus()
+            Call Kode_Otomatis()
+        Else
+            With tblUser.Rows(0)
+                TxtKdPengguna.Text = .Item("User_Id")
+                TxtNmPengguna.Text = .Item("Username")
+                TxtPassword.Text = .Item("Password")
+                TxtPassword.Enabled = False
+                TxtUlangPassword.Enabled = False
+                CboLevel.Text = .Item("Role")
+            End With
+            BtnSimpan.Enabled = False
+            BtnUbah.Enabled = True
+            BtnHapus.Enabled = True
+            BtnBatal.Enabled = True
+        End If
+    End Sub
+
+    Private Sub DGPengguna_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DgPengguna.CellClick
+        Dim i As Integer
+
+        With DgPengguna
+            If e.RowIndex >= 0 Then
+                i = .CurrentRow.Index
+
+                TxtKdPengguna.Text = .Rows(i).Cells("User_Id").Value.ToString
+                TxtNmPengguna.Text = .Rows(i).Cells("Username").Value.ToString
+
+                BtnUbah.Enabled = True
+                BtnHapus.Enabled = True
+                BtnBatal.Enabled = True
+                BtnSimpan.Enabled = False
+            End If
+        End With
+    End Sub
+    Private Sub FrmUser_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Left = 0
+        Top = 0
+        Call Bersih()
+        CboLevel.Items.Add("ADMIN")
+        CboLevel.Items.Add("OPERATOR")
+    End Sub
+End Class
